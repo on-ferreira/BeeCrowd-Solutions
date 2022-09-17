80c04d3fac1dc1803968604f0c62ebb3550bd9be
on-ferreira <73672733+on-ferreira@users.noreply.github.com>
2022-09-17 17:53:26 -0300

    Create 1067.c

---
+++ 1067.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+	int a,x=0;
+	scanf("%d",&a);
+   	x=1;
+   	
+   	if(a>=1 && a<=1000);
+   	{
+	while(x<=a)
+   	{
+   		printf("%d\n",x);
+   		x = x+2;
+   		}
+   	}
+    return 0;
+}
