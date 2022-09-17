390f0c5faa19675cfa30a79ca9e134f93018e9b5
on-ferreira <73672733+on-ferreira@users.noreply.github.com>
2022-09-17 18:04:11 -0300

    Create 1008.c

---
+++ 1008.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+	int a,b;
+	double c;
+    scanf("%d" "%d" "%lf" , &a, &b, &c);
+    double d=b*c;
+    printf("NUMBER = %.d\nSALARY = U$ %.2lf\n",a,d);
+    return 0;
+}
